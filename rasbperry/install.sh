2a886c660f1ede2b5b79dbd8b325abb101d91f4f
gektor650 <gektor650@gmail.com>
2024-04-26 13:34:30 -0600

    Initial commit

--- rasbperry/install.sh
+++ rasbperry/install.sh
@@ -1,4 +1,4 @@
-# sudo apt install raspberrypi-kernel 
+#!/bin/bash
 
 sudo apt-get install -y git \
     python3 
@@ -9,5 +9,18 @@ pip3 install websockets --break-system-packages
 # pip3 install dataclasses-json --break-system-packages
 # sudo nano /boot/firmware/config.txt
 
-sudo bash -c 'echo "dtoverlay=pwm-2chan,pin=12,func=4,pin2=13,func2=4" >> /boot/firmware/config.txt'
+# TODO: Uncomment
+#sudo bash -c 'echo "dtoverlay=pwm-2chan,pin=12,func=4,pin2=13,func2=4" >> /boot/firmware/config.txt'
+
+# Get the current kernel version
+current_kernel=$(uname -r)
+
+# https://github.com/Pioreactor/rpi_hardware_pwm/issues/14
+if [[ "$current_kernel" == "6.6.20+rpt-rpi-2712" ]]; then
+  sudo apt install raspberrypi-kernel
+  echo "Kernel version matches! Installing raspberrypi-kernel..."
+else
+  echo "Kernel version does not match (current: $current_kernel)"
+fi
+
 sudo reboot now
