eb33b32e7ad0ffb9a3253bef01127b70bbb4ec8b
gektor650 <gektor650@gmail.com>
2024-04-26 13:24:06 -0600

    Initial commit

--- rasbperry/pwm_test.py
+++ rasbperry/pwm_test.py
@@ -6,8 +6,6 @@ from pwm_control import Channel
 channels: list[Channel] = [
     Channel.GPIO_12, 
     Channel.GPIO_13,
-    Channel.GPIO_18,
-    Channel.GPIO_19,
 ]
 
 pwm_control = PwmControl()
@@ -19,12 +17,9 @@ for channel in channels:
 print("Set 100%")
 pwm_control.set(Channel.GPIO_12, 50)
 pwm_control.set(Channel.GPIO_13, 0)
-# pwm_control.set(Channel.GPIO_18, 100)
-# pwm_control.set(Channel.GPIO_19, 100)
+
 time.sleep(3)
 
 print("Set 0%")
 pwm_control.set(Channel.GPIO_12, 0)
 pwm_control.set(Channel.GPIO_13, 0)
-# pwm_control.set(Channel.GPIO_18, 0)
-# pwm_control.set(Channel.GPIO_19, 0)
